475a4bd332abf2066f0954ac867b76be2bffdc1d
Georgi Parlakov <gparlakov@gmail.com>
2018-12-15 22:38:51 +0200

    made the tests run much faster excluding the Material design modules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MailedBooksModule } from './mailed-books/mailed-books.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,17 +11,18 @@ describe('AppComponent', () => {
         AppComponent
       ],
       imports: [
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        MailedBooksModule
-      ]
+        BrowserAnimationsModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'mbs'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
--- src/app/mailed-books/mailed-books.component.spec.ts
+++ src/app/mailed-books/mailed-books.component.spec.ts
@@ -1,31 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MailedBooksComponent } from './mailed-books.component';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('MailedBooksComponent', () => {
-  let component: MailedBooksComponent;
-  let fixture: ComponentFixture<MailedBooksComponent>;
+    let component: MailedBooksComponent;
+    let fixture: ComponentFixture<MailedBooksComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MailedBooksComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    //   imports: [MatButtonModule, MatListModule]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MailedBooksComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MailedBooksComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MailedBooksComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(fixture.elementRef.nativeElement).toMatchSnapshot();
-
-  });
+    it('should create', () => {
+        expect(fixture.elementRef.nativeElement).toMatchSnapshot();
+    });
 });
