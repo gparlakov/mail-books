adcecccf80fcfb6af481d9d495ba780593c0fc21
Georgi Parlakov <gparlakov@gmail.com>
2018-12-15 15:35:50 +0200

    Add jest to the mix

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    preset: "jest-preset-angular",
+    setupTestFrameworkScriptFile: "<rootDir>/src/setupJest.ts",
+    roots: ['<rootDir>/src'],
+    modulePaths: ['<rootDir>/node_modules']
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { MailedBooksModule } from './mailed-books/mailed-books.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MailedBooksModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MailedBooksModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
